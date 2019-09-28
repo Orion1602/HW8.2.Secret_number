1e0efecc8402fdf793ccec2a111f5a038729deed
Stephanie Leeb <stephanieleeb@gmx.at>
2019-09-28 19:22:47 +0200

    Added some text

---
+++ main.py
@@ -0,0 +1,9 @@
+secret = "16"
+
+guess = input("What's the secret number between 0 and 20??")
+
+if guess == secret:
+    print("You got it!!!! Hooray!!")
+
+else:
+    print("That was a fail. Try again!!")
