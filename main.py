81397cbf01d3cebfe0b152c8addecde106ad6486
Stephanie Leeb <stephanieleeb@gmx.at>
2019-10-05 08:08:08 +0200

    Update

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-secret = "16"
+import random
+secret = random.randint(1, 20)
 
-guess = input("What's the secret number between 0 and 20??")
 
-if guess == secret:
-    print("You got it!!!! Hooray!!")
+for x in range (6):
+   guess = int(input("What's the secret number between 0 and 20??"))
 
-else:
-    print("That was a fail. Try again!!")
+   if guess == secret:
+        print("You got it!!!! Hooray!!")
+        break
+   elif guess > secret:
+       print("Your guess is not correct.... try something smaller!!")
+   elif guess < secret:
+       print("Your guess is not correct.... try something bigger!!")
+   else:
+        print("That was a fail. Try again!!")
